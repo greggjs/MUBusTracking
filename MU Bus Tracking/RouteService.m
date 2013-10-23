de75ecf3f101c1a067b77e15ef93e4627d80e352
Jake Gregg <greggjs@miamioh.edu>
2013-10-23 15:22:14 -0400

    Zooming works now, scales perfectly.

--- MU Bus Tracking/RouteService.m
+++ MU Bus Tracking/RouteService.m
@@ -70,54 +70,32 @@
 -(CLLocationCoordinate2D) determineWindowSizeWithRoute:(Route*)route withCooridnates:(NSArray*)retpoints{
     CLLocationCoordinate2D temp;
     [[retpoints objectAtIndex:0]getValue:&temp];
-    CLLocationCoordinate2D top_left = temp;
-    //NSLog(@"%f, %f", top_left.latitude, top_left.longitude);
-    CLLocationCoordinate2D top_right = temp;
-    CLLocationCoordinate2D bottom_left = temp;
-    CLLocationCoordinate2D bottom_right = temp;
-    CLLocationCoordinate2D  p1, p2, center;
-    //NSLog(@"top_left has: %f, %f", top_left.latitude, top_left.longitude);
+    CLLocationCoordinate2D left = temp, right= temp, top = temp, bottom = temp, center;
     route.shape = [[NSArray alloc] initWithArray:retpoints];
     for (int i = 1; i < [route.shape count]; i++) {
         CLLocationCoordinate2D curr;
         [[retpoints objectAtIndex:i]getValue:&curr];
-        if (curr.latitude > top_left.latitude
-            && curr.longitude < top_left.longitude) {
-            top_left = curr;
-        } if (curr.latitude < bottom_right.latitude
-              && curr.longitude > bottom_right.longitude) {
-            bottom_right = curr;
-        } if (curr.latitude < top_right.latitude
-              && curr.longitude > top_right.longitude) {
-            top_right = curr;
-            
-        } if (curr.latitude < bottom_left.latitude
-              && curr.longitude < bottom_left.longitude) {
-            bottom_left = curr;
+        if (curr.latitude > top.latitude) {
+            top = curr;
+        } if (curr.latitude < bottom.latitude) {
+            bottom = curr;
+        } if (curr.longitude > right.longitude) {
+            right = curr;
+        } if (curr.longitude < left.longitude) {
+            left = curr;
         }
     }
-    if (top_left.latitude < bottom_left.latitude) {
-        p1 = bottom_left;
-        p2 = top_right;
-    } else {
-        p1 = top_left;
-        p2 = bottom_right;
-    }
-    center.latitude = (p1.latitude+p2.latitude)/2;
-    center.longitude = (p1.longitude+p2.longitude)/2;
+    center.latitude = (top.latitude+bottom.latitude)/2;
+    center.longitude = (left.longitude+right.longitude)/2;
     
-    float west = p1.longitude;
-    float east = p2.longitude;
-    float angle = east - west;
-    
+    float angle = right.longitude - left.longitude;
     if (angle < 0)
         angle += 360.0;
     CGRect screen = [[UIScreen mainScreen]bounds];
     CGFloat scale = [[UIScreen mainScreen] scale];
     CGSize size = CGSizeMake(screen.size.width * scale, screen.size.height * scale);
-    route.zoom = floor(log(size.width*360/angle/256)/0.693)-0.75;
-    //NSLog(@"%f", log(size.width*360/angle/256));
-    NSLog(@"%f, %f, %f", center.latitude, center.longitude, route.zoom);
+    route.zoom = log(size.width*360/angle/256)/0.693-1.2;
+    //NSLog(@"%f, %f, %f", center.latitude, center.longitude, route.zoom);
     return center;
 }
 @end
