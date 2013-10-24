553666af3a1abb906617f132e10e24a57eae4960
Pete <petemk@egb-10-33-10-196.muo.muohio.edu>
2013-10-23 23:05:46 -0400

    LeftSizeBar now has color image and a heading(route.name) and subheading (route.longname)

--- MU Bus Tracking/SidebarViewController.m
+++ MU Bus Tracking/SidebarViewController.m
@@ -60,14 +60,42 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        //cell.textLabel.font          = [UIFont fontWithName:@"Helvetica-Bold" size:18.0];
+        //cell.textLabel.lineBreakMode = UILineBreakModeWordWrap;
+        cell.textLabel.numberOfLines = 0;
+       // [cell.textLabel setMinimumFontSize:13.0];
+       // [cell.textLabel setAdjustsFontSizeToFitWidth:NO];
     }
     cell.textLabel.textColor = [UIColor darkTextColor];
     cell.backgroundColor = [UIColor clearColor];
     
+    
     if (indexPath.row == 0)
         cell.textLabel.text = @"All Routes";
-    else
-        cell.textLabel.text = ((Route*)_routes[indexPath.row-1]).longname;
+    else {
+        Route *route = (Route*)_routes[indexPath.row-1];
+        
+        //Create Header Font & String
+        NSString *headerString = [NSString stringWithFormat: @"%@\n", route.name];
+        UIFont *Header = [UIFont fontWithName:@"Helvetica-Bold" size:18.0];
+        NSDictionary *headerDict = [NSDictionary dictionaryWithObject: Header forKey: NSFontAttributeName];
+        NSMutableAttributedString *cellString = [[NSMutableAttributedString alloc] initWithString:headerString attributes: headerDict];
+        
+        //Create sub information
+        UIFont *subHeading = [UIFont fontWithName:@"Helvetica" size:10.0];
+        NSDictionary *subHeadingDict = [NSDictionary dictionaryWithObject: subHeading forKey: NSFontAttributeName];
+        NSMutableAttributedString *detailInfo = [[NSMutableAttributedString alloc] initWithString:route.longname attributes: subHeadingDict];
+        [cellString appendAttributedString:detailInfo];
+        cell.textLabel.attributedText = cellString;
+        
+        //GetSize for image creation
+        CGSize headerSize = [self getCellSizesWithText:headerString andFont:Header];
+        CGSize subHeadingSize = [self getCellSizesWithText:route.longname andFont:subHeading];
+        CGSize cellSize = CGSizeMake(headerSize.height + subHeadingSize.height, 5);
+
+        UIImage *image = [self imageWithColor:route.color andSize:cellSize];
+        cell.imageView.image = image;
+    }
     /*
     UIImage *image;
     
@@ -96,14 +124,45 @@
             image = [UIImage imageNamed:@"bus_red.png"];
             break;
     }
-     
-    cell.imageView.image = image;
     */
+    
     return cell;
 }
+/*
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellText    = @"some text which is part of cell display";
+    UIFont *cellFont      = [UIFont fontWithName:@"Helvetica-Bold" size:18];
+    CGSize constraintSize = CGSizeMake(280.0f, MAXFLOAT);
+    CGSize labelSize      = [cellText sizeWithFont:cellFont constrainedToSize:constraintSize lineBreakMode:UILineBreakModeWordWrap];
+    //int buffer  = 10;
+    return labelSize.height + buffer;
+}
+*/
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     return self.title;
+}
+#pragma mark - private methods
+
+-(CGSize) getCellSizesWithText:(NSString *)cellText andFont:(UIFont *)cellFont {
+    CGSize constraintSize = CGSizeMake(280.0f, MAXFLOAT);
+    CGSize cellSize      = [cellText sizeWithFont:cellFont constrainedToSize:constraintSize lineBreakMode:UILineBreakModeWordWrap];
+    return cellSize;
+}
+
+- (UIImage *)imageWithColor:(UIColor *)color andSize:(CGSize)size {
+    CGRect rect = CGRectMake(0.0f, 0.0f, size.height, size.width);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
 }
 
 #pragma mark - Table view delegate
